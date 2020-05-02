7b18b7e5a5e09b110ef4c4c6d671e73445a044d3
med-chhoub <chhoub.mohamed@gmail.com>
2020-05-02 22:30:54 +0000

    init projects

---
+++ movie-infos-service/src/main/java/com/microservices/training/movieinfosservice/MovieInfosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.training.movieinfosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfosServiceApplication.class, args);
+	}
+
+}
---
+++ movie-infos-service/src/test/java/com/microservices/training/movieinfosservice/MovieInfosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.training.movieinfosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
